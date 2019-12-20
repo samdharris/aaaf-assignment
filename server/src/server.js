cba7144aef9452d01cf13348e159715e0fe1676c
Sam D Harris <samdharris@outlook.com>
2019-12-20 12:11:43 +0000

    setup body parser

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 dotenv.config();
 
 const server = express();
+
+server.use(bodyParser.json());
+server.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 server.listen(process.env.PORT, _ => {
     console.log(`Listening on port ${process.env.PORT}`);
