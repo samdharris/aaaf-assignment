b50b1c03dc644b0f8481afb2db67fd62dd4a64a4
Sam D Harris <samdharris@outlook.com>
2019-12-30 18:45:18 +0000

    Show snackbar on successful login

--- client/src/store/modules/auth/actions.js
+++ client/src/store/modules/auth/actions.js
@@ -1,4 +1,5 @@
 import { SET_AUTHENTICATING, SET_ERRORS, SET_CURRENT_USER } from "./auth-types";
+import { SET_SNACKBAR } from "../general/general-types";
 import axios from "../../../util/axios";
 
 export default {
@@ -11,6 +12,15 @@ export default {
 
             localStorage.setItem("token", data.token);
             ctx.commit(SET_CURRENT_USER, data.user);
+            ctx.commit(
+                `general/${SET_SNACKBAR}`,
+                {
+                    color: "success",
+                    text: "You are now logged in!",
+                    open: true
+                },
+                { root: true }
+            );
         } catch (error) {
             ctx.commit(SET_ERRORS, error.response.data);
         } finally {
