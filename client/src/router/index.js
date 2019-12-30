512bc12eb73d4373a33be57afe35d07f6b36db59
Sam D Harris <samdharris@outlook.com>
2019-12-30 21:08:19 +0000

    move routes to their own file

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,37 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Login from "../views/Login.vue";
+import routes from "./routes";
 
 import { isAuthenticated } from "../util/authHelper";
 Vue.use(VueRouter);
-
-const routes = [
-    {
-        path: "/",
-        component: Home,
-        meta: {
-            requiresAuth: true,
-            visitWithAuth: true
-        }
-    },
-    {
-        path: "/about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "about" */ "../views/About.vue")
-    },
-    {
-        path: "/login",
-        component: Login,
-        meta: {
-            requiresAuth: false,
-            visitWithAuth: false
-        }
-    }
-];
 
 const router = new VueRouter({
     mode: "history",
@@ -48,14 +20,14 @@ router.beforeEach((to, from, next) => {
         record => record.meta.visitWithAuth
     );
 
-    if (routeRequiresAuth && !userAuthenticated) {
+    if (routeRequiresAuth && userAuthenticated === false) {
         next({
             path: "/login"
         });
         return;
     }
 
-    if (userAuthenticated && !routeCanBeViewedWhenLoggedIn) {
+    if (userAuthenticated && routeCanBeViewedWhenLoggedIn === false) {
         next({
             path: "/"
         });
