dbb7168f63a203f8c8bedcf69c9ff2e11600c9e4
Sam D Harris <samdharris@outlook.com>
2019-12-30 16:27:41 +0000

    Put the login form in a card

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,54 +1,56 @@
 <template>
     <v-container fill-height>
-        <v-row align-content="center">
-            <v-col>
-                <h1>Login</h1>
-            </v-col>
-        </v-row>
         <v-row>
             <v-col>
-                <ValidationObserver
-                    v-slot="{ invalid, validated, passes, validate }"
-                >
-                    <v-form>
-                        <ValidationProvider
-                            name="email"
-                            rules="required|email"
-                            v-slot="{ errors, valid }"
+                <v-card class="elevation-12">
+                    <v-toolbar dark color="primary">
+                        <v-toolbar-title>Login</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                        <ValidationObserver
+                            v-slot="{ invalid, validated, passes, validate }"
                         >
-                            <v-text-field
-                                v-bind="$attrs"
-                                v-model="loginData.email"
-                                type="email"
-                                :error-messages="errors"
-                                :success="valid"
-                                id="email"
-                                name="email"
-                                label="email"
-                            ></v-text-field>
-                        </ValidationProvider>
-                        <ValidationProvider
-                            name="password"
-                            rules="required"
-                            v-slot="{ errors, valid }"
-                        >
-                            <v-text-field
-                                required
-                                :error-messages="errors"
-                                :success="valid"
-                                v-model="loginData.password"
-                                label="Password"
-                                type="password"
-                            ></v-text-field>
-                        </ValidationProvider>
-                        <v-btn
-                            color="success"
-                            @click="passes(onSubmit)"
-                            :disabled="invalid && validated"
-                            >Login</v-btn
-                        >
-                    </v-form>
-                </ValidationObserver>
+                            <v-form>
+                                <ValidationProvider
+                                    name="email"
+                                    rules="required|email"
+                                    v-slot="{ errors, valid }"
+                                >
+                                    <v-text-field
+                                        v-bind="$attrs"
+                                        v-model="loginData.email"
+                                        type="email"
+                                        :error-messages="errors"
+                                        :success="valid"
+                                        id="email"
+                                        name="email"
+                                        label="email"
+                                    ></v-text-field>
+                                </ValidationProvider>
+                                <ValidationProvider
+                                    name="password"
+                                    rules="required"
+                                    v-slot="{ errors, valid }"
+                                >
+                                    <v-text-field
+                                        required
+                                        :error-messages="errors"
+                                        :success="valid"
+                                        v-model="loginData.password"
+                                        label="Password"
+                                        type="password"
+                                    ></v-text-field>
+                                </ValidationProvider>
+                                <v-btn
+                                    color="success"
+                                    @click="passes(onSubmit)"
+                                    :disabled="invalid && validated"
+                                    >Login</v-btn
+                                >
+                            </v-form>
+                        </ValidationObserver>
+                    </v-card-text>
+                </v-card>
             </v-col>
         </v-row>
     </v-container>
