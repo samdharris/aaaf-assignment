7ac91b8da6dcfc451135f947a160976fb55389c7
Sam D Harris <samdharris@outlook.com>
2020-01-26 13:38:18 +0000

    Add an isAdmin flag against user

--- server/src/database/models/user.model.js
+++ server/src/database/models/user.model.js
@@ -21,6 +21,10 @@ const userSchema = new Schema(
             type: Boolean,
             default: false,
         },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
         profilePic: {
             type: String,
             default: null,
