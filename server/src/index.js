9abbc7b2e416457aa73b58f8c6e8c580ce6031e2
Sam D Harris <samdharris@outlook.com>
2020-01-29 17:05:44 +0000

    Shuffle boot

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,10 @@
 const server = require('./server');
-
-server.start();
+const database = require('./database');
+const logger = require('./logger');
+const dotenv = require('dotenv');
+dotenv.config();
+database.connect().then(() => {
+    server.listen(process.env.PORT, _ => {
+        logger.info(`Server running on port: ${process.env.PORT}`);
+    });
+});
--- server/src/server.js
+++ server/src/server.js
@@ -1,43 +1,27 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const router = require('./routes');
-const database = require('./database');
 const cors = require('cors');
-const logger = require('./logger');
 const fileUpload = require('express-fileupload');
-dotenv.config();
 
 const server = express();
 
-server.start = async () => {
-    try {
-        await database.connect();
+server.use(
+    fileUpload({
+        safeFileNames: true,
+        preserveExtension: true,
+        debug: process.env.NODE_ENV !== 'production',
+        createParentPath: true,
+    })
+);
+server.use(cors());
+server.use(bodyParser.json());
+server.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
-        server.use(
-            fileUpload({
-                safeFileNames: true,
-                preserveExtension: true,
-                debug: process.env.NODE_ENV !== 'production',
-                createParentPath: true,
-            })
-        );
-        server.use(cors());
-        server.use(bodyParser.json());
-        server.use(
-            bodyParser.urlencoded({
-                extended: true,
-            })
-        );
-
-        server.use(router);
-
-        server.listen(process.env.PORT, _ => {
-            logger.info(`Server running on port: ${process.env.PORT}`);
-        });
-    } catch (e) {
-        console.error(`Failed to boot: ${e.message}`);
-    }
-};
+server.use(router);
 
 module.exports = server;
