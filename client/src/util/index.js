0d6544032e2292ec597914ca014b62c1988b8e7e
Sam D Harris <samdharris@outlook.com>
2020-02-08 18:00:51 +0000

    better

--- client/src/util/index.js
+++ client/src/util/index.js
@@ -1,9 +1,13 @@
 import moment from "moment";
 const dateFormat = "DD/MM/YYYY HH:mm:ss";
 export const format = data => {
-    return {
-        ...data,
-        createdAt: moment(data.createdAt).format(dateFormat),
-        updatedAt: moment(data.updatedAt).format(dateFormat)
-    };
+    const newData = { ...data };
+    if (typeof data.createdAt !== "undefined") {
+        data.createdAt = moment(data.createdAt).format(dateFormat);
+    }
+
+    if (typeof data.createdAt !== "undefined") {
+        data.updatedAt = moment(data.updatedAt).format(dateFormat);
+    }
+    return newData;
 };
