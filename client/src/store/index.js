d061d26af1517079f878d88d65cdd4288948e183
Sam D Harris <samdharris@outlook.com>
2020-02-16 14:42:09 +0000

    Document auth

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+// Vuex modules
 import auth from "./modules/auth";
 import general from "./modules/general";
 import teams from "./modules/teams";
@@ -20,14 +21,29 @@ export default new Vuex.Store({
         chat
     },
     actions: {
+        /**
+         * Action that is called when a user is connected to the chat server
+         * @param {*} ctx
+         * @param {*} data
+         */
         "SOCKET_user-connected"(ctx, data) {
             ctx.commit("chat/SEND_MESSAGE", {
                 text: data
             });
         },
+        /**
+         * Action that is called when a message has been sent and recieved
+         * @param {*} ctx
+         * @param {*} data
+         */
         "SOCKET_message-sent"(ctx, data) {
             ctx.commit("chat/SEND_MESSAGE", data);
         },
+        /**
+         * Action called when a user disconnects from the chat server
+         * @param {*} ctx
+         * @param {*} data
+         */
         "SOCKET_user-disconnected"(ctx, data) {
             ctx.commit("chat/SEND_MESSAGE", {
                 text: data
--- client/src/store/modules/auth/mutations.js
+++ client/src/store/modules/auth/mutations.js
@@ -1,11 +1,26 @@
 import { SET_AUTHENTICATING, SET_ERRORS, SET_CURRENT_USER } from "./auth-types";
 export default {
+    /**
+     * Sets isAuthenticating flag
+     * @param {object} state
+     * @param {bool} isAuthenticating
+     */
     [SET_AUTHENTICATING](state, isAuthenticating) {
         state.isAuthenticating = isAuthenticating;
     },
+    /**
+     * Sets any errors during authentication to show to the user
+     * @param {object} state
+     * @param {object} errors
+     */
     [SET_ERRORS](state, errors) {
         state.errors = { ...errors };
     },
+    /**
+     * Sets the current user upon login
+     * @param {object} state
+     * @param {object} user
+     */
     [SET_CURRENT_USER](state, user) {
         state.currentUser = { ...user };
     }
