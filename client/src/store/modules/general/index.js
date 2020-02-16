1e323823cc73aa7cb008ea932190b7f50f95b572
Sam D Harris <samdharris@outlook.com>
2020-02-16 16:50:25 +0000

    Document general module

--- client/src/store/modules/general/index.js
+++ client/src/store/modules/general/index.js
@@ -2,6 +2,9 @@ import actions from "./actions";
 import state from "./state";
 import mutations from "./mutations";
 import getters from "./getters";
+/**
+ * General App Module. For anything not specific
+ */
 export default {
     namespaced: true,
     actions,
--- client/src/store/modules/general/mutations.js
+++ client/src/store/modules/general/mutations.js
@@ -1,8 +1,17 @@
 import { SET_SNACKBAR, RESET_SNACKBAR } from "./general-types";
 export default {
+    /**
+     * Setup the snackbar with the given config
+     * @param {object} state
+     * @param {object} config
+     */
     [SET_SNACKBAR](state, config) {
         state.snackbar = { ...config };
     },
+    /**
+     * Reset the snackbar state to its default config
+     * @param {object} state
+     */
     [RESET_SNACKBAR](state) {
         const defaultConfig = { text: "", color: "", open: false };
         state.snackbar = { ...defaultConfig };
