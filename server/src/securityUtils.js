048074955ee04c9409cae2a7c79f83ee244de3a1
Sam D Harris <samdharris@outlook.com>
2020-02-16 14:31:15 +0000

    Document security utils functions

--- server/src/securityUtils.js
+++ server/src/securityUtils.js
@@ -1,25 +1,55 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+/**
+ * Generates a hashed password
+ *
+ * @param {string} openPass
+ * @returns string
+ */
 exports.hashPassword = async openPass => {
     const salt = await genSalt();
     const password = await bcrypt.hash(openPass, salt);
     return password;
 };
 
+/**
+ * Checks hashes match
+ *
+ * @param {string} clear
+ * @param {string} hash
+ * @returns Promise<bool>
+ */
 exports.hashesAreSame = async (clear, hash) =>
     await bcrypt.compare(clear, hash);
 
+/**
+ * Generates a JWT token
+ *
+ * @param {object} payload
+ * @returns string
+ */
 exports.generateToken = payload => {
     return jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: '60m',
     });
 };
 
+/**
+ * Verfies the given JWT token
+ *
+ * @param {string} token
+ * @returns object
+ */
 exports.validateToken = token => {
     return jwt.verify(token, process.env.JWT_SECRET, {
         maxAge: '60m',
     });
 };
 
+/**
+ * Generates a salt for the hash
+ *
+ * @returns Promise<string>
+ */
 const genSalt = async () => await bcrypt.genSalt(10);
