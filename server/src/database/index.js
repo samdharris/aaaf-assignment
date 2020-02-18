af49f300d948de46e5211fae3e487f570cb4ae84
Sam D Harris <samdharris@outlook.com>
2020-02-18 13:37:31 +0000

    Swap out DB config to use the full URI

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 const logger = require('../logger');
 
 exports.connect = async () => {
-    await mongoose.connect(
-        `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0-gui1q.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 
     logger.info('Connected to mongo!');
 };
